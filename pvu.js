c88344ddfa83104c96220d1c8a51d2d7ff11d905
Daybrush <daybrush@gmail.com>
2019-11-15 23:20:42 +0900

    fix: fix relative path

--- pvu.js
+++ pvu.js
@@ -1,47 +1,45 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const args = require('args');
-const path = require('path');
-const fs = require('fs');
+const args = require("args");
+const path = require("path");
+const fs = require("fs");
 const { splitUnit } = require("@daybrush/utils");
-const { shell } = require('./utils');
+const { shell } = require("./utils");
 
 args
-    .option('path', 'package folder', "packages")
-    .option('packages', 'packages', "")
+    .option("path", "package folder", "packages")
+    .option("packages", "packages", "");
 
 const {
     path: packagesPath,
     packages,
 } = args.parse(process.argv);
 
-
 const cwd = process.cwd();
 const { name, version } = JSON.parse(fs.readFileSync(path.resolve(cwd, "package.json"), { encoding: "utf8" }));
 const versions = version.split(".");
 
 packages.split(",").forEach(packagePath => {
-    const packageFullPath = path.resolve(packagesPath, packagePath);
-    const packageJSONPath = ath.resolve(packageFullPath, "package.json");
-    const packageJSON = fs.readFileSync(packageJSONPath);
+    const packagesFullPath = path.resolve(packagesPath, packagePath);
+    const packageJSONPath = path.resolve(packagesFullPath, "package.json");
+    const packageJSON = fs.readFileSync(packageJSONPath, { encoding: "utf-8" });
 
-    const
     const {
         version: packageVersion,
         dependencies: {
             [name]: originalVersion,
         },
     } = JSON.parse(packageJSON);
-    const originalVersions = originalVersion.split(".");
+    const originalVersions = originalVersion.replace(/^[^\d]?/g, "").split(".");
     const packageVersions = packageVersion.split(".");
 
-    const changedIndex = originalVersions.find((v, i) => v !== verions[i]);
+    const changedIndex = originalVersions.find((v, i) => v !== versions[i]);
 
     if (changedIndex === -1) {
         return;
     }
-    const ov = splitUnit(originalVerions[changedIndex]);
+    const ov = splitUnit(originalVersions[changedIndex]);
     const vv = splitUnit(versions);
 
     if (ov.value === vv.value) {
@@ -53,7 +51,8 @@ packages.split(",").forEach(packagePath => {
             packageVersions[i] = 0;
         }
     }
+    const relativePath = path.relative(cwd, packagesFullPath);
     fs.writeFileSync(packageJSONPath, packageJSON.replace(packageVersion, packageVersions.join(".")), { encoding: "utf-8" });
-    shell(`cd ${packageFullPath} && npm i ${name}@latest`);
-    shell(`cd ${packageFullPath.split("/").map(v => v ? ".." : "").join("/")}/`);
+    shell(`cd ${relativePath} && npm i ${name}@latest`);
+    shell(`cd ${relativePath.split("/").map(v => v ? ".." : "").join("/")}/`);
 });
